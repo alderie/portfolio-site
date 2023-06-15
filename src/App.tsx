a892c9983631acf744eca3de509aa4add8db79cc
Victor <victornovich@yahoo.com>
2023-06-15 13:50:17 -0400

    Improved font loading, added better touch support for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -36,39 +36,112 @@ function App() {
   const [section, setSection, sectionRef] = useStateRef(0);
   const [projectNum, setProjectNum, projectNumRef] = useStateRef(0);
 
-  const increasePagePosition = debounce(500, () => {
-    if (sectionRef.current === 1 && projectNumRef.current < NUM_PROJECTS) {
-      setProjectNum(Math.min(projectNumRef.current + 1, NUM_PROJECTS));
-    } else {
+  /**
+   * Debounced section and project index setters
+   */
+  const increaseSectionNum = useRef(
+    debounce(500, () => {
       setSection(Math.min(sectionRef.current + 1, BREAKPOINTS.length - 1));
-    }
-  });
-  const decreasePagePosition = debounce(500, () => {
-    if (sectionRef.current === 1 && projectNumRef.current > 0) {
-      setProjectNum(Math.max(projectNumRef.current - 1, 0));
-    } else {
+    })
+  );
+  const increaseProjectNum = useRef(
+    debounce(500, () => {
+      setProjectNum(Math.min(projectNumRef.current + 1, NUM_PROJECTS));
+    })
+  );
+  const decreaseSectionNum = useRef(
+    debounce(500, () => {
       setSection(Math.max(sectionRef.current - 1, 0));
-    }
-  });
+    })
+  );
+  const decreaseProjectNum = useRef(
+    debounce(500, () => {
+      setProjectNum(Math.max(projectNumRef.current - 1, 0));
+    })
+  );
 
   const appRef = useRef<HTMLDivElement>(null);
+  const initialPosition = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
+  const offset = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
 
   useEffect(() => {
+
+    // Handle mousewheel scroll
     const onScroll = (evt: WheelEvent) => {
       // Scroll down
       if (evt.deltaY > 0) {
-        increasePagePosition();
+        if (sectionRef.current === 1 && projectNumRef.current < NUM_PROJECTS) {
+          increaseProjectNum.current();
+        } else {
+          increaseSectionNum.current();
+        }
       } else if (evt.deltaY < 0) {
-        decreasePagePosition();
+        if (sectionRef.current === 1 && projectNumRef.current > 0) {
+          decreaseProjectNum.current();
+        } else {
+          decreaseSectionNum.current();
+        }
+      }
+    };
+
+    // Handle touch events for mobile and tablet
+    const onTouchBegin = (evt: TouchEvent) => {
+      if (evt.touches.length === 0) return;
+
+      const touch = evt.touches[0];
+      initialPosition.current = { x: touch.clientX, y: touch.clientY };
+    };
+
+    const onTouchMove = (evt: TouchEvent) => {
+      const touch = evt.touches[0];
+      offset.current = {
+        x: touch.clientX - initialPosition.current.x,
+        y: touch.clientY - initialPosition.current.y,
+      };
+    };
+
+    const onTouchEnd = (evt: TouchEvent) => {
+      const THRESHOLD = 50;
+      // Special handling of swiping in the x direction for the projects section
+      if (sectionRef.current === 1) {
+
+        // Only change sections if y offset is over threshold
+        if (offset.current.y < -THRESHOLD && Math.abs(offset.current.x) < THRESHOLD && projectNumRef.current === NUM_PROJECTS) {
+          increaseSectionNum.current();
+        } else if (offset.current.y > THRESHOLD && Math.abs(offset.current.x) < THRESHOLD && projectNumRef.current === 0) {
+          decreaseSectionNum.current();
+        }
+
+        if (offset.current.x < -THRESHOLD) {
+          increaseProjectNum.current();
+        } else if (offset.current.x > THRESHOLD) {
+          decreaseProjectNum.current();
+        }
+
+        // Every other section is treated normally with no x direction swiping
+      } else {
+        if (offset.current.y < 0) {
+          increaseSectionNum.current();
+        } else if (offset.current.y > 0) {
+          decreaseSectionNum.current();
+        }
       }
     };
 
     window.addEventListener("wheel", onScroll);
 
+    window.addEventListener("touchstart", onTouchBegin);
+    window.addEventListener("touchmove", onTouchMove);
+    window.addEventListener("touchend", onTouchEnd);
+
+    // Remove all listener on component destructuring
     return () => {
       window.removeEventListener("wheel", onScroll);
+      window.removeEventListener("touchstart", onTouchBegin);
+      window.removeEventListener("touchmove", onTouchMove);
+      window.removeEventListener("touchend", onTouchEnd);
     };
-  }, [increasePagePosition, decreasePagePosition]);
+  }, [increaseProjectNum, increaseSectionNum, decreaseProjectNum, decreaseSectionNum, projectNumRef, sectionRef]);
 
   useEffect(() => {
     if (appRef.current) {
@@ -91,13 +164,18 @@ function App() {
       </div>
       <div className="splash">
         <h1 className="splash-text">
-          <b>VICTOR</b><span>NOVICHKOV</span>
+          <b>VICTOR</b>
+          <span>NOVICHKOV</span>
         </h1>
         <div className="description">
           <div className="subtext">
             design - development - <b className="accent">magic</b>
           </div>
-          <img src={mySvg} className="subtext-decor" alt="colorful brushstrokes" />
+          <img
+            src={mySvg}
+            className="subtext-decor"
+            alt="colorful brushstrokes"
+          />
         </div>
       </div>
       <ContactButton></ContactButton>
